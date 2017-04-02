3325128d97a139c011176092d2fdde8956d87fb5
NightFury <82142029+nightfury-dev@users.noreply.github.com>
2017-04-02 14:26:32 +0300

    update react-native and react-native-vector-icons

--- android/app/src/main/java/com/discgolfapp/MainApplication.java
+++ android/app/src/main/java/com/discgolfapp/MainApplication.java
@@ -1,10 +1,10 @@
 package com.discgolfapp;
 
 import android.app.Application;
-import android.util.Log;
 
 import com.facebook.react.ReactApplication;
-import com.facebook.react.ReactInstanceManager;
+import io.realm.react.RealmReactPackage;
+import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -17,14 +17,16 @@ public class MainApplication extends Application implements ReactApplication {
 
   private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
     @Override
-    protected boolean getUseDeveloperSupport() {
+    public boolean getUseDeveloperSupport() {
       return BuildConfig.DEBUG;
     }
 
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage()
+          new MainReactPackage(),
+            new RealmReactPackage(),
+            new VectorIconsPackage()
       );
     }
   };
