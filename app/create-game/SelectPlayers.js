6ec0a63a3ebe8b607f2c0d8c4bf7126ebf231ec6
NightFury <82142029+nightfury-dev@users.noreply.github.com>
2017-04-10 20:15:44 +0300

    add ability to add a new player during game creation

--- app/create-game/SelectPlayers.js
+++ app/create-game/SelectPlayers.js
@@ -1,9 +1,13 @@
 import * as _ from 'lodash';
 import React from 'react';
 import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 import { ScrollView, View, ListView, Text, StyleSheet } from 'react-native';
 import { Button } from 'native-base';
 
+import { addPlayer } from '../actions/actionCreators';
+import AddPlayerModal from '../shared/components/AddPlayerModal';
+import AddActionButton from '../shared/components/AddActionButton';
 import PlayerListElement from '../shared/components/PlayerListElement';
 import styles from './styles/SelectPlayersStyles';
 
@@ -12,34 +16,46 @@ class SelectPlayers extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      players: this.props.players.map((p) =>
-                _.extend({ selected: false }, p)
-            )
+      selectedPlayers: {},
+      showAddPlayerDialog: false
     };
 
+    this.addPlayer = this.addPlayer.bind(this);
+    this.hideAddPlayerDialog = this.hideAddPlayerDialog.bind(this);
     this.renderRow = this.renderRow.bind(this);
     this.playersSelected = this.playersSelected.bind(this);
   }
 
+  addPlayer(name) {
+    this.props.addPlayer(name);
+    this.hideAddPlayerDialog();
+  }
+
+  hideAddPlayerDialog() {
+    this.setState({
+      showAddPlayerDialog: false
+    });
+  }
+
   playersSelected() {
-    const selectedPlayers = this.state.players.filter((p) => p.selected);
+    const selectedPlayers = this.props.players.filter(
+      (p) => this.state.selectedPlayers[p.id]
+    );
     this.props.playersSelected(selectedPlayers);
   }
 
   handleSelection(player) {
-    const i = _.findIndex(this.state.players, (p) => p.id === player.id);
-    const players = [
-      ...this.state.players.slice(0, i),
-            { ...player, selected: !player.selected },
-      ...this.state.players.slice(i + 1)
-    ];
-    this.setState({ players });
+    const selectedPlayers = {
+      ...this.state.selectedPlayers,
+      [player.id]: !this.state.selectedPlayers[player.id]
+    };
+    this.setState({ selectedPlayers });
   }
 
   renderRow(rowData) {
     return (<PlayerListElement
       player={rowData}
-      selected={rowData.selected}
+      selected={this.state.selectedPlayers[rowData.id]}
       onPress={() => this.handleSelection(rowData)}
     />);
   }
@@ -53,30 +69,41 @@ class SelectPlayers extends React.Component {
   render() {
     const dataSource = new ListView.DataSource({
       rowHasChanged: (r1, r2) => r1.name !== r2.name
-    }).cloneWithRows(this.state.players);
-    const isPlayersSelected = _.some(this.state.players, (p) => p.selected);
+    }).cloneWithRows(this.props.players);
+    const isPlayersSelected = _.some(this.state.selectedPlayers, p => p);
     return (
-      <ScrollView>
-        <ListView
-          dataSource={dataSource}
-          renderRow={this.renderRow}
-          renderSeparator={this.renderSeparator}
+      <View>
+        <AddPlayerModal
+          onSave={this.addPlayer}
+          onCancel={this.hideAddPlayerDialog}
+          visible={this.state.showAddPlayerDialog}
         />
-        <View style={{ flexDirection: 'row', justifyContent: 'center' }}>
-          <Button
-            disabled={!isPlayersSelected}
-            style={StyleSheet.flatten(styles.button)}
-            onPress={this.playersSelected}
-          >
-            <Text style={styles.baseText}>Continue</Text>
-          </Button>
-        </View>
-      </ScrollView>
+        <ScrollView>
+          <ListView
+            dataSource={dataSource}
+            renderRow={this.renderRow}
+            renderSeparator={this.renderSeparator}
+          />
+          <View style={{ flexDirection: 'row', justifyContent: 'center' }}>
+            <Button
+              disabled={!isPlayersSelected}
+              style={StyleSheet.flatten(styles.button)}
+              onPress={this.playersSelected}
+            >
+              <Text style={styles.baseText}>Continue</Text>
+            </Button>
+          </View>
+        </ScrollView>
+        <AddActionButton
+          onPress={() => this.setState({ showAddPlayerDialog: true })}
+        />
+      </View>
     );
   }
 }
 
 SelectPlayers.propTypes = {
+  addPlayer: React.PropTypes.func.isRequired,
   players: React.PropTypes.array.isRequired,
   playersSelected: React.PropTypes.func.isRequired
 };
@@ -85,4 +112,8 @@ const mapStateToProps = (state) => ({
   players: state.players
 });
 
-export default connect(mapStateToProps)(SelectPlayers);
+const mapDispatchToProps = (dispatch) => ({
+  addPlayer: bindActionCreators(addPlayer, dispatch)
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(SelectPlayers);
