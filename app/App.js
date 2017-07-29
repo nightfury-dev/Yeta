db7e740d62fcb398a659992e9abb4557b805de4c
NightFury <82142029+nightfury-dev@users.noreply.github.com>
2017-07-29 14:07:20 +0300

    export navigation into own component

--- app/App.js
+++ app/App.js
@@ -3,35 +3,12 @@ import './ReactotronConfig';
 /* eslint import/imports-first: 0 */
 import React from 'react';
 import { connect, Provider } from 'react-redux';
-import { Router, Scene } from 'react-native-router-flux';
 
-import Menu from './menu';
-import Players from './players';
-import Courses from './courses';
-import InitGame from './create-game';
-import ResumeGame from './resume-game';
-import Game from './game';
-import AddCourse from './course-form';
-import Scorecard from './scorecard';
+import Navigation from './navigation';
 import createStore from './redux';
 import StartupActions from './redux/StartupRedux';
 
 const store = createStore();
-
-const Navigation = () => (
-  <Router>
-    <Scene key={'root'}>
-      <Scene key={'menu'} component={Menu} initial />
-      <Scene key={'players'} component={Players} />
-      <Scene key={'initgame'} component={InitGame} />
-      <Scene key={'resumegame'} component={ResumeGame} />
-      <Scene key={'game'} component={Game} />
-      <Scene key={'courses'} component={Courses} />
-      <Scene key={'addcourse'} component={AddCourse} />
-      <Scene key={'scorecard'} component={Scorecard} />
-    </Scene>
-  </Router>
-);
 
 class RootComponent extends React.Component {
   componentWillMount() {
---
+++ app/navigation/Navigation.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Router, Scene } from 'react-native-router-flux';
+
+import Menu from '../menu';
+import Players from '../players';
+import Courses from '../courses';
+import InitGame from '../create-game';
+import ResumeGame from '../resume-game';
+import Game from '../game';
+import AddCourse from '../course-form';
+import Scorecard from '../scorecard';
+
+const Navigation = () => (
+  <Router>
+    <Scene key={'root'}>
+      <Scene key={'menu'} component={Menu} initial />
+      <Scene key={'players'} component={Players} />
+      <Scene key={'initgame'} component={InitGame} />
+      <Scene key={'resumegame'} component={ResumeGame} />
+      <Scene key={'game'} component={Game} />
+      <Scene key={'courses'} component={Courses} />
+      <Scene key={'addcourse'} component={AddCourse} />
+      <Scene key={'scorecard'} component={Scorecard} />
+    </Scene>
+  </Router>
+);
+
+export default Navigation;
